33070d0f2fafb45d9fb2eb831d46f99f3ded5c21
Abinaya <abinayamuthumurugan999@gmail.com>
2025-02-25 12:36:06 +0530

    Create Emp.java

---
+++ Employee/Emp.java
@@ -0,0 +1,33 @@
+package employee;
+import java.util.Scanner;
+class Emp
+{
+double c1(double bp)
+{
+return bp*0.5;
+}
+double c2(double bp)
+{
+return bp*0.2;
+}
+double c3(double bp,double hra,double da)
+{
+return bp+hra+da;
+}
+double c4(double bp)
+{
+return bp*0.5;
+}
+double c5(double bp,double np)
+{
+return bp+np;
+}
+double c6(double gp)
+{
+return gp*0.2;
+}
+double c7(double it,double gp)
+{
+return gp-it;
+}
+}
